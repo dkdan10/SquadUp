b53fd60551b00977474cbcc595029bcdaeb937be
Daniel Keinan <danielkeinan0@gmail.com>
2019-08-02 19:21:07 -0400

    Adjust footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { destroySession } from '../../actions/session_actions';
 
 class Footer extends React.Component {
     
+    goTo (url) {
+        return e => {
+            this.props.history.push(url)
+        }
+    }
+
     render () {
+        const login_logout = this.props.currentUser ? (
+            <>
+            <span onClick={() => this.props.logout()}>Log out</span>
+            <span>Profile</span>
+            <span onClick={this.goTo("/groups/new")}>Create a Group</span>
+            </>
+        ) : (
+            <span onClick={this.goTo("/login")}>Log in</span >
+        )
+
         return (
             <div className="footer-container">
+                <div className="left-side">
+                    <div className="logo">SquadUp</div>
+                    <div className="links-info">    
+                        <span className="stay-connected">STAY CONNECTED</span>
+                        <div className="the-links">
+                            <a href="https://www.linkedin.com/in/daniel-keinan-ab9a5a148/"><i className="fab fa-linkedin-in"></i></a>
+                            <a href="https://github.com/dkdan10/SquadUp"><i className="fab fa-github"></i></a>
+                        </div>
+                        <div className="email">danielkeinan@icloud.com</div>
+                    </div>
+                </div>
 
+                <div className="right-side">
+                    <div className="explore">Explore</div>
+                    <div className="explore-links">
+                        {login_logout}
+                    </div>
+
+                </div>
+                
             </div>
         )
     }
@@ -19,4 +56,8 @@ const mapStateToProps = state => {
     }
 };
 
-export default connect(mapStateToProps)(Footer)
+const mapDispatchToProps = dispatch => ({
+    logout: () => dispatch(destroySession()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Footer))
