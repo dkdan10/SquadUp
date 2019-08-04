a8ed757de07d1ee10552e30202de26b0e95affda
Daniel Keinan <danielkeinan0@gmail.com>
2019-08-04 14:35:42 -0400

    Edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,23 +31,23 @@ u5 = User.create!(username: "Ram", email: "ram@ram.com", password: "password", l
 u6 = User.create!(username: "Betty", email: "bet@betty.com", password: "password", location_id: l5.id)
 demoU = User.create!(username: "DemoUser", email: "demo@user.com", password: "password", location_id: l1.id)
 
-g1 = Group.create!(name:"First Group", description:"This is the first group", location_id: l1.id, private: false, owner_id: u1.id)
+g1 = Group.create!(name:"First Group", description:"This is the first group. It is for people who want to be in the first group. The first group is a great time!", location_id: l1.id, private: false, owner_id: u1.id)
 g1.members << u1
 g1.members << u2
-g2 = Group.create!(name:"Second Group", description:"This is the second group", location_id: l3.id, private: false, owner_id: u2.id)
+g2 = Group.create!(name:"Second Group", description:"This is the second group. As the saying goes: First is the worst second is the best! ", location_id: l3.id, private: false, owner_id: u2.id)
 g2.members << u2
-g3 = Group.create!(name:"Third Group", description:"This is the third group", location_id: l5.id, private: false, owner_id: u3.id)
+g3 = Group.create!(name:"Third Group", description:"This is the third group. The third group is for people didn't want to join the first or second group.", location_id: l5.id, private: false, owner_id: u3.id)
 g3.members << u3
-g4 = Group.create!(name:"Toyko Basketballers", description:"Lets all Squad up and play basketball together!", location_id: l3.id, private: false, owner_id: u1.id)
+g4 = Group.create!(name:"Toyko Basketballers", description:"Lets all Squad up and play basketball together! Any skill level is welcome. Come through with a smile!", location_id: l3.id, private: false, owner_id: u1.id)
 g4.members << u4
-g5 = Group.create!(name:"Ram's social hour!", description:"This group is for friend's of Ram to hang out with Ram.", location_id: l1.id, private: false, owner_id: u5.id)
+g5 = Group.create!(name:"Ram's social hour!", description:"This group is for friend's of Ram to hang out with Ram. It should be a blast. Stay tuned!", location_id: l1.id, private: false, owner_id: u5.id)
 g5.members << u1
 g5.members << u2
 g5.members << u3
 g5.members << u4
 g5.members << u5
 g5.members << u6
-g6 = Group.create!(name:"Betty's Crafts!", description:"Come and do arts and crafts with Betty!", location_id: l1.id, private: false, owner_id: u6.id)
+g6 = Group.create!(name:"Betty's Crafts!", description:"Come and do arts and crafts with Betty! We will have a great time. I can't wait to meet new people", location_id: l1.id, private: false, owner_id: u6.id)
 g6.members << u1
 g6.members << u2
 g6.members << u3
