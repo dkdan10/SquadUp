54168168564a55d997184c546d4df3ac0d4cc89f
Daniel Keinan <danielkeinan0@gmail.com>
2019-09-30 12:31:18 -0400

    minor change

--- frontend/actions/join_group_actions.js
+++ frontend/actions/join_group_actions.js
@@ -2,12 +2,12 @@ import * as JoinGroupAPI from "../util/join_group_api"
 import { recieveGroup } from "./group_actions";
 
 
-export const addMemberToGroup = (groupId) => {
+export const addMemberToGroup = (groupId) => (dispatch) => {
     return JoinGroupAPI.addMemberToGroup(groupId)
         .then(groupData => dispatch(recieveGroup(groupData)))
 }
 
-export const removeMemberFromGroup = (groupId) => {
+export const removeMemberFromGroup = (groupId) => (dispatch) => {
     return JoinGroupAPI.removeMemberFromGroup(groupId)
         .then(groupData => dispatch(recieveGroup(groupData)))
 }
